e574a7a9167f39af5e6742400ba100d23816c4e6
Timur Kohut <kogt55695@gmail.com>
2026-04-05 16:26:23 +0300

    prepare django for railway

--- rentora/settings.py
+++ rentora/settings.py
@@ -4,7 +4,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-change-this-key'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.up.railway.app",
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -61,7 +64,7 @@ LANGUAGE_CODE = 'uk'
 TIME_ZONE = 'Europe/Kyiv'
 USE_I18N = True
 USE_TZ = True
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
